78667cd6ba8ac05a6898fc4bf09e2584422b57c2
Mohammed-altahir <75260113+Mohammed-altahir@users.noreply.github.com>
2025-03-25 18:28:53 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -141,9 +141,9 @@ def download():
 
 
 # API endpoint for scanning (Non-blocking)
-@app.post("/scan",response_class=HTMLResponse)
-async def scan_target(device_name:str = Form(...), device_type:str = Form(...), ip_address: str = Form(...)):
-    await perform_scan(ip_address,device_name,device_type)
+@app.post("/scan")
+async def scan_target(background_tasks: BackgroundTasks, device_name:str = Form(...), device_type:str = Form(...), ip_address: str = Form(...)):
+    background_tasks.add_task(perform_scan, ip_address,device_name,device_type)
     print(f"Scan started in the background\nIP address: {ip_address}")
     return RedirectResponse(url="/service", status_code=303)
 
